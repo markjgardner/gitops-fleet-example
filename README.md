d10a5e546650c0dbb0451c7231fa722ee100b02d
Mark Gardner <mark.gardner@microsoft.com>
2024-12-12 16:05:13 +0000

    sort of works

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # gitops-fleet-example
 An example of leveraging gitops with fleet manager to operate multiple clusters declaratively
+
+# TODO
+fluxconfiguration does not reconcile because CAPZ doesn't import the ASO CRD
--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 LOCATION=eastus2
 FLEET_MEMBER_GROUP=myfleet-rg
 FLEET_HUB_GROUP=hub-rg
-ASO_CLUSTER=asocluster-aks
+CAPZ_CLUSTER=capzcluster-aks
 FLEET_CLUSTER=hub-fleet
 
 # Create resource groups
@@ -11,60 +11,55 @@ az group create -n $FLEET_MEMBER_GROUP -l $LOCATION
 
 # Create fleet hub
 az fleet create -g $FLEET_HUB_GROUP -n $FLEET_CLUSTER -l $LOCATION --enable-hub --enable-managed-identity
+az role assignment create --role "Azure Kubernetes Fleet Manager Contributor Role" --assignee $(az account show --query user.name -o tsv) --scope /subscriptions/$SUBID/resourceGroups/$FLEET_HUB_GROUP
 
 # Create a managed identity with necessary permissions
 SUBID=$(az account show -o tsv --query id)
 TENANT=$(az account show -o tsv --query tenantId)
-IDENTITY=$(az identity create -g $FLEET_HUB_GROUP -n aso-aks-contributor -o tsv --query clientId)
-az role assignment create --role "Reader" --assignee $IDENTITY --scope /subscriptions/$SUBID/resourceGroups/$FLEET_MEMBER_GROUP
-az role assignment create --role "Azure Kubernetes Service Contributor Role" --assignee $IDENTITY --scope /subscriptions/$SUBID/resourceGroups/$FLEET_MEMBER_GROUP
-az role assignment create --role "Kubernetes Extension Contributor" --assignee $IDENTITY --scope /subscriptions/$SUBID/resourceGroups/$FLEET_MEMBER_GROUP
-az role assignment create --role "Reader" --assignee $IDENTITY --scope /subscriptions/$SUBID/resourceGroups/$FLEET_HUB_GROUP
-az role assignment create --role "Azure Kubernetes Fleet Manager Contributor Role" --assignee $IDENTITY --scope /subscriptions/$SUBID/resourceGroups/$FLEET_HUB_GROUP
+CAPZ_MI=capz-contributor
+CLIENTID=$(az identity create -g $FLEET_HUB_GROUP -n $CAPZ_MI -o tsv --query clientId)
+az role assignment create --role "Contributor" --assignee $CLIENTID --scope /subscriptions/$SUBID/resourceGroups/$FLEET_MEMBER_GROUP
+az role assignment create --role "Reader" --assignee $CLIENTID --scope /subscriptions/$SUBID/resourceGroups/$FLEET_HUB_GROUP
+az role assignment create --role "Azure Kubernetes Fleet Manager Contributor Role" --assignee $CLIENTID --scope /subscriptions/$SUBID/resourceGroups/$FLEET_HUB_GROUP
 
 
-# Create an AKS cluster that will serve as the ASO host
-ASOID=$(az aks create -g $FLEET_HUB_GROUP -n $ASO_CLUSTER --node-count 1 --enable-oidc-issuer --enable-workload-identity --generate-ssh-keys -o tsv --query id)
-ISSUER=$(az aks show -g $FLEET_HUB_GROUP -n $ASO_CLUSTER -o json | jq -r '.oidcIssuerProfile.issuerUrl')
+# Create an AKS cluster that will serve as the CAPZ cluster
+CAPZ_CLUSTER_ID=$(az aks create -g $FLEET_HUB_GROUP -n $CAPZ_CLUSTER --node-count 1 --enable-oidc-issuer --enable-workload-identity --generate-ssh-keys -o tsv --query id)
+ISSUER=$(az aks show -g $FLEET_HUB_GROUP -n $CAPZ_CLUSTER -o json | jq -r '.oidcIssuerProfile.issuerUrl')
 
-# Establish Trust between the AKS cluster and the managed identity
-az identity federated-credential create --name aso-federated-credential \
-    --identity-name aso-aks-contributor -g $FLEET_HUB_GROUP \
+# Establish Trust between the CAPZ ans ASO operators and the managed identity
+az identity federated-credential create --name capz-federated-credential \
+    --identity-name $CAPZ_MI -g $FLEET_HUB_GROUP \
     --issuer $ISSUER \
-    --subject "system:serviceaccount:azureserviceoperator-system:azureserviceoperator-default" \
+    --subject "system:serviceaccount:capz-system:capz-manager" \
+    --audiences "api://AzureADTokenExchange"
+az identity federated-credential create --name aso-federated-credential \
+    --identity-name $CAPZ_MI -g $FLEET_HUB_GROUP \
+    --issuer $ISSUER \
+    --subject "system:serviceaccount:capz-system:azureserviceoperator-default" \
     --audiences "api://AzureADTokenExchange"
 
-# Install Azure Service Operator on the cluster
-az aks get-credentials -g $FLEET_HUB_GROUP -n $ASO_CLUSTER --overwrite-existing
+# Install CAPZ on the cluster
+az aks get-credentials -g $FLEET_HUB_GROUP -n $CAPZ_CLUSTER --overwrite-existing
 
-# ASO requires cert manager
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.14.1/cert-manager.yaml
-
-# Helm install ASOv2
-helm repo add aso2 https://raw.githubusercontent.com/Azure/azure-service-operator/main/v2/charts
-helm upgrade --install aso2 aso2/azure-service-operator \
-        --create-namespace \
-        --namespace=azureserviceoperator-system \
-        --set azureSubscriptionID=$SUBID \
-        --set azureTenantID=$TENANT \
-        --set azureClientID=$IDENTITY \
-        --set useWorkloadIdentityAuth=true \
-        --set crdPattern='resources.azure.com/*;containerservice.azure.com/*;kubernetesconfiguration.azure.com/*'
+# The following envvars are used to configure the managed identity used by capz and aso
+export AZURE_TENANT_ID=$TENANT
+export AZURE_SUBSCRIPTION_ID=$SUBID
+export AZURE_CLIENT_ID=$CLIENTID
+# TODO: verify this work otherwise you have to explicitly set them in the secrets post capz init
+clusterctl init --infrastructure azure
 
 # Join the cluster to the fleet
-az fleet member create -g $FLEET_HUB_GROUP -f $FLEET_CLUSTER -n $ASO_CLUSTER --member-cluster-id $ASOID
+az fleet member create -g $FLEET_HUB_GROUP -f $FLEET_CLUSTER -n $CAPZ_CLUSTER --member-cluster-id $CAPZ_CLUSTER_ID
 
-# Label the cluster as an ASO host
+# Label the cluster as the CAPZ cluster
 az fleet get-credentials -g $FLEET_HUB_GROUP -n $FLEET_CLUSTER --overwrite-existing
-kubectl label cluster $ASO_CLUSTER clusterType=aso
+kubectl label cluster $CAPZ_CLUSTER clusterType=capz
 
 # define clusters
 kubectl create ns cluster-definitions
 kubectl apply -f ./fleet/cluster-definitions/
 
-# define flux config
-kubectl create ns cluster-config
-kubectl apply -f ./fleet/flux/
 
 # Install the CRPs
 kubectl apply -f ./fleet/placements/
